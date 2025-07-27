b59259772bbff3b389dca4dae9c512d0df38193c
KomX <komx512@outlook.com>
2025-07-27 22:27:26 +0500

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
@@ -12,6 +13,31 @@ public class Main {
         final int GENERATION_LENGHT = 100;
 
         List<Thread> threadsList = new ArrayList<>();
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
+        Thread observerThread = new Thread(() -> {
+            int maxkey = 0;
+            while (!Thread.interrupted()) {
+                synchronized (sizeToFreq) {
+                    int currentMaxKey = 0;
+                    try {
+                        sizeToFreq.wait();
+                    } catch (InterruptedException e) {
+                        //e.printStackTrace();
+                        break;
+                    }
+
+                    currentMaxKey = sizeToFreq.keySet().stream().max(Integer::compare).get();
+
+                    if (currentMaxKey > maxkey) {
+                        maxkey = currentMaxKey;
+                        System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", maxkey, sizeToFreq.get(maxkey));
+                    }
+                }
+            }
+        });
+
+        observerThread.start();
 
         for (int i = 0; i < ATTEMTS; i++) {
 
@@ -20,6 +46,7 @@ public class Main {
                 int rCount = charCount(charsLine);
                 synchronized (sizeToFreq) {
                     sizeToFreq.put(rCount, sizeToFreq.getOrDefault(rCount, 0) + 1);
+                    sizeToFreq.notify();
                 }
             });
 
@@ -27,17 +54,19 @@ public class Main {
             thread.start();
         }
 
-        for (Thread current : threadsList){
+        for (Thread current : threadsList) {
             current.join();
         }
 
-        int maxKey = sizeToFreq.keySet().stream().max(Integer::compare).get();
-        System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", maxKey, sizeToFreq.get(maxKey));
-        sizeToFreq.entrySet()
-                .stream()
-                .filter(current -> !current.getKey().equals(maxKey))
-                .sorted(Map.Entry.<Integer, Integer>comparingByKey().reversed())
-                .forEach(current -> System.out.printf(" - %d (%d раз) \n", current.getKey(), current.getValue()));
+        observerThread.interrupt();
+
+//       int maxKey = sizeToFreq.keySet().stream().max(Integer::compare).get();
+//        System.out.printf("Самое частое количество повторений %d (встретилось %d раз)\n", maxKey, sizeToFreq.get(maxKey));
+//        sizeToFreq.entrySet()
+//                .stream()
+//                .filter(current -> !current.getKey().equals(maxKey))
+//                .sorted(Map.Entry.<Integer, Integer>comparingByKey().reversed())
+//                .forEach(current -> System.out.printf(" - %d (%d раз) \n", current.getKey(), current.getValue()));
     }
 
     public static String generateRoute(String letters, int length) {
